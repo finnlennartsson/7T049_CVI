71f294d1275406a9c857ec9887fe260a1f923818
Finn Lennartsson <finn.lennartsson@gmail.com>
2021-12-20 16:28:29 +0100

    some changes

--- dMRI/preprocess.sh
+++ dMRI/preprocess.sh
@@ -16,8 +16,8 @@ Arguments:
 Options:
   -dwiAP			dMRI AP data (default: rawdata/sub-sID/dwi/sub-sID_dir-AP_run-1_dwi.nii.gz)
   -dwiPA			dMRI PA data, potentially for TOPUP  (default: rawdata/sub-sID/dwi/sub-sID_dir-PA_run-1_dwi.nii.gz)
-  -seAP				Spin-echo field map AP, for TOPUP (default: rawdata/sub-sID/fmap/sub-sID_acq-se_dir-AP_run-1_epi.nii.gz)
-  -sePA				Spin-echo field map PA, for TOPUP (default: rawdata/sub-sID/fmap/sub-sID_acq-se_dir-PA_run-1_epi.nii.gz)
+  -b0APvol			b0 volume in dMRI AP data to use in TOPUP (default: 0)
+  -b0PAvol			b0 volume in dMRI PA data to use in TOPUP (default: 0)
   -d / -data-dir  <directory>   The directory used to output the preprocessed files (default: derivatives/dMRI/sub-sID)
   -t / -threads	  		Number of threads	  		
   -h / -help / --help           Print usage.
@@ -38,8 +38,8 @@ studydir=$PWD
 # Defaults
 dwiAP=rawdata/sub-$sID/dwi/sub-${sID}_dir-AP_run-1_dwi.nii.gz
 dwiPA=rawdata/sub-$sID/dwi/sub-${sID}_dir-PA_run-1_dwi.nii.gz
-seAP=rawdata/sub-$sID/fmap/sub-${sID}_acq-se_dir-AP_run-1_epi.nii.gz
-sePA=rawdata/sub-$sID/fmap/sub-${sID}_acq-se_dir-PA_run-1_epi.nii.gz
+b0APvol=0
+b0PAvol=0
 datadir=derivatives/dMRI/sub-$sID
 
 # check whether the different tools are set and load parameters
@@ -49,8 +49,8 @@ while [ $# -gt 0 ]; do
     case "$1" in
 	-dwiAP) shift; dwi=$1; ;;
 	-dwiPA) shift; dwiPA=$1; ;;
-	-seAP) shift; seAP=$1; ;;
-	-seAP) shift; sePA=$1; ;;
+	-b0APvol) shift; b0APvol=$1; ;;
+	-b0PAvol) shift; b0PAvol=$1; ;;
 	-d|-data-dir)  shift; datadir=$1; ;;
 	-h|-help|--help) usage; ;;
 	-*) echo "$0: Unrecognized option $1" >&2; usage; ;;
@@ -62,15 +62,13 @@ done
 # Check if images exist, else put in No_image
 if [ ! -f $dwiAP ]; then dwiAP=""; fi
 if [ ! -f $dwiPA ]; then dwiPA=""; fi
-if [ ! -f $seAP ]; then seAP=""; fi
-if [ ! -f $sePA ]; then sePA=""; fi
 
 echo "Registration and sMRI-processing
 Subject:       $sID 
 DWI (AP):      $dwiAP
 DWI (PA):      $dwiPA
-SE fMAP (AP):  $seAP	       
-SE fMAP (PA):  $sePA	       
+b0APvol:       $b0APvol
+b0PAvol:       $b0PAvol	       
 Directory:     $datadir
 Threads:       $threads 
 $BASH_SOURCE   $command
@@ -119,9 +117,9 @@ sePA=`basename $sePA .nii.gz`
 
 
 ##################################################################################
-# 0. Create dwi.mif.gz to work with in /preproc
+# 0. Create dwi.mif.gz to work with in /preproc and b0APPA.mif.gz in /preproc/topup
 
-if [ ! -d $datadir/dwi/preproc ]; then mkdir -p $datadir/dwi/preproc; fi
+if [ ! -d $datadir/dwi/preproc/topup ]; then mkdir -p $datadir/dwi/preproc/topup; fi
 
 cd $datadir
 
@@ -131,27 +129,70 @@ if [[ $dwiAP = "" ]];then
 else
     # Create a dwiAP.mif.gz-file to work with
     if [ ! -f dwi/preproc/dwiAP.mif.gz ]; then
-	mrconvert -json_import dwi/orig/$dwiAP.json -fslgrad dwi/orig/$dwiAP.bvec dwi/orig/$dwiAP.bval -import_pe_table $codedir/../sequences/petable_dwi_acq-se_dir-AP_dwi.txt dwi/orig/$dwiAP.nii.gz dwi/preproc/dwiAP.mif.gz
+	mrconvert -strides -1,2,3,4 -json_import dwi/orig/$dwiAP.json -fslgrad dwi/orig/$dwiAP.bvec dwi/orig/$dwiAP.bval -import_pe_table $codedir/../sequences/petable_dwi_acq-se_dir-AP_dwi.txt dwi/orig/$dwiAP.nii.gz dwi/preproc/dwiAP.mif.gz
     fi
 fi
 
-if [[ $dwiPA = "" ]];then
-    echo "No dwiAP data provided";
+if [[ $dwiPA = "" ]]; then
+    echo "No dwiPA data provided";
     exit;
 else
     # Create a dwiPA.mif.gz-file to work with
     if [ ! -f dwi/preproc/dwiPA.mif.gz ]; then
-	mrconvert -json_import dwi/orig/$dwiPA.json -fslgrad dwi/orig/$dwiPA.bvec dwi/orig/$dwiPA.bval -import_pe_table $codedir/../sequences/petable_dwi_acq-se_dir-PA_dwi.txt dwi/orig/$dwiPA.nii.gz dwi/preproc/dwiPA.mif.gz
+	mrconvert -strides -1,2,3,4 -json_import dwi/orig/$dwiPA.json -fslgrad dwi/orig/$dwiPA.bvec dwi/orig/$dwiPA.bval -import_pe_table $codedir/../sequences/petable_dwi_acq-se_dir-PA_dwi.txt dwi/orig/$dwiPA.nii.gz dwi/preproc/dwiPA.mif.gz
     fi
 fi
 
-# We only use dwiAP in dwi.mif.gz
-# Look into incorporating dwiPA as well
+# Split data into data for different shells 
 cd dwi/preproc
+
 if [ ! -f dwi.mif.gz ]; then
-    ln -s dwiAP.mif.gz dwi.mif.gz
+
+    for dir in AP PA; do
+
+	# 1. extract higher shells and put in a joint file
+	if [ $dir == AP ]; then
+	    dwiextract -shells 1000,2600 dwi$dir.mif.gz tmp_dwi${dir}_b1000b2600.mif
+	fi
+	if [ $dir == PA ]; then
+	    dwiextract -shells 1000 dwi$dir.mif.gz tmp_dwi${dir}_b1000.mif
+	fi
+
+	# 2. extract the b0 that will be used for TOPUP by
+	# a) noting correct volume
+	if [ $dir == AP ]; then b0topup=$b0APvol; fi
+	if [ $dir == PA ]; then b0topup=$b0PAvol; fi
+	# b) and put in /topup/tmp_b0$dir.mif
+	mrconvert -coord 3 $b0topup -axes 0,1,2 dwi$dir.mif.gz topup/tmp_b0$dir.mif
+	# c) and extract b0s from dwi$dir.mif where the b0 for TOPUP will be placed first (by creating and an indexlist)
+	indexlist=$b0topup;
+	for index in `mrinfo -shell_indices dwi$dir.mif.gz | awk '{print $1}' | sed 's/\,/\ /g'`; do
+	    if [ ! $index == $b0topup ]; then
+		indexlist=`echo $indexlist,$index`;
+	    fi
+	    echo $indexlist;
+	done
+	mrconvert -coord 3 $indexlist dwi$dir.mif.gz tmp_dwi${dir}_b0.mif
+	
+    done
+    
+    # Put everything into file dwi.mif.gz, with AP followed by PA volumes
+    # FL 2021-12-20 - NOTE TOPUP and EDDY not working properly for dirPA, so only use dirAP to go into dwi.mif.gz
+    mrcat -axis 3 tmp_dwiAP_b0.mif tmp_dwiAP_b1000b2600.mif dwi.mif.gz
+
+    # clean-up
+    rm tmp_dwi*.mif
+    
 fi
 
+# Sort out topup/b0APPA.mif.gz
+cd topup
+if [ ! -f b0APPA.mif.gz ]; then
+    mrcat -axis 3 tmp_b0AP.mif tmp_b0PA.mif b0APPA.mif.gz
+    #clean-up
+    rm tmp_dwi*_b0.mif
+fi
+    
 cd $studydir
 
 ##################################################################################
@@ -190,10 +231,13 @@ cd $studydir
 
 cd $datadir/dwi/preproc
 
-if [ ! -d topup ]; then mkdir topup; fi
+if [ ! -d topup ]; then mkdir -p topup; fi
+if [ ! -d eddy ]; then mkdir -p eddy; fi
+
+cd topup
 
 # Create b0APPA.mif.gz to go into TOPUP
-if [ ! -f topup/b0APPA.mif.gz ];then
+if [ ! -f b0APPA.mif.gz ];then
     echo "Create a PErevPE pair of SE images to use with TOPUP
 1. Do this by put one good b0 from dir-AP_dwi and dir-PA_dwi into a file b0APPA.mif.gz into $datadir/dwi/preproc/topup
 2. Run this script again.    
@@ -201,57 +245,56 @@ if [ ! -f topup/b0APPA.mif.gz ];then
     exit;
 fi
 
-# Create a brain mask for EDDY to go into dwifslpreproc
-
-if [ ! -f eddy_mask.mif.gz ]; then
-    # NOTE - normally dwifslpreproc creates mask from dwi2mask
-    # Instead make mask with BET from as the union av BET from meanb1000AP and meanb2600AP to ensure sufficient coverage/extent
-    for bvalue in 0 1000 2600; do
-	dwiextract -force -shells $bvalue -strides -1,2,3,4 dwiAP.mif.gz - | mrmath -axis 3 - mean meanb${bvalue}APtmp.nii.gz
-	bet meanb${bvalue}APtmp.nii.gz meanb${bvalue}APtmp_brain.nii.gz -R -F -m
-    done
-    for bvalue in 0 1000; do
-	dwiextract -force -shells $bvalue -strides -1,2,3,4 dwiPA.mif.gz - | mrmath -axis 3 - mean meanb${bvalue}PAtmp.nii.gz
-	bet meanb${bvalue}PAtmp.nii.gz meanb${bvalue}PAtmp_brain.nii.gz -R -F -m
-    done
-    # make union av brain masks and save into eddy_mask.mif.gz
-    mrcat meanb*tmp_brain_mask.nii.gz - | mrmath - mean -axis 3 - | mrcalc - 1 -ge - | maskfilter -npass 3 -force - dilate eddy_mask.mif.gz
-    #clean-up
-    rm meanb*APtmp*
-fi
-if [ ! -f eddy_mask_edit.mif.gz ]; then
-    echo Check brain mask for EDDY and save into eddy_mask_edit.mif.gz
-    cp eddy_mask.mif.gz eddy_mask_edit.mif.gz
-    mrview topup/b0APPA.mif.gz -roi.load eddy_mask_edit.mif.gz -mode 2
-    exit;
+# create input files from b0APPA.mif.gz
+if [ ! -f topup_in.nii.gz ]; then
+    mrconvert b0APPA.mif.gz topup_in.nii.gz -strides -1,+2,+3,+4 -export_pe_table topup_datain.txt
 fi
 
-scratchdir=dwifslpreproc
-if [ ! -f dwi_den_unr_eddy.mif.gz ];then
-    dwifslpreproc -se_epi topup/b0APPA.mif.gz -rpe_pair -pe_dir ap -readout_time 0.0312 -align_seepi \
-		  -nocleanup \
-		  -scratch $scratchdir \
-		  -eddy_mask eddy_mask_edit.mif.gz \
-		  -eddy_slspec $codedir/../sequences/slspec_MB1_60slices_interleaved.txt \
-		  -topup_options " --iout=field_mag_unwarped" \
-		  -eddy_options " --slm=linear --repol --mporder=16 --s2v_niter=10 --s2v_interp=trilinear --s2v_lambda=1 " \
-		  -eddyqc_all eddy \
-		  dwi_den_unr.mif.gz \
-		  dwi_den_unr_eddy.mif.gz;
-   # or use -rpe_pair combo: dwifslpreproc DWI_in.mif DWI_out.mif -rpe_pair -se_epi b0_pair.mif -pe_dir ap -readout_time 0.72 -align_seepi
+# RUN TOPUP
+if [ ! -f field_mag_unwarped.nii.gz ]; then 
+    echo running TOPUP
+    topup --imain=topup_in.nii.gz --datain=topup_datain.txt --out=field --fout=field_fieldmap.nii.gz --config=$FSLDIR/etc/flirtsch/b02b0.cnf --verbose --iout=field_mag_unwarped
+    fslmaths field_mag_unwarped.nii.gz -Tmean field_magnitude.nii.gz
 fi
-# Now cleanup by transferring relevant files to qc, eddy and topup folder and deleting scratch folder
-mv eddy/quad ../../qc/.
-cp $scratchdir/command.txt $scratchdir/log.txt $scratchdir/eddy_*.txt $scratchdir/applytopup_*.txt $scratchdir/slspec.txt eddy/.
-if [ ! -d topup ]; then mkdir topup; fi
-mv $scratchdir/field_* $scratchdir/topup_* topup/.
-rm -rf $scratchdir 
+# Create a mask
+if [ ! -f field_magnitude_brain_mask.nii.gz ]; then
+    bet field_magnitude.nii.gz field_magnitude_brain.nii.gz -m -R -F
+fi
+
+cd ../eddy
+
+# Create brain mask eddy_mask_edit.nii.gz to go inte EDDY.
+if [ ! -f eddy_mask_edit.nii.gz ]; then
+    # use brain mask from topup 
+    mrconvert -datatype float32 ../topup/field_magnitude_brain_mask.nii.gz eddy_mask.nii.gz
+    echo edit brain mask and save into eddy/eddy_mask.nii.gz
+    mrview ../topup/field_magnitude.nii.gz -roi.load eddy_mask.nii.gz -roi.opacity 0.5 -mode 2
+    
+fi
+
+# Create input to EDDY
+if [ ! -f eddy_in.nii.gz ]; then
+    mrconvert -strides -1,2,3,4 ../dwi_den_unr.mif.gz eddy_in.nii.gz -export_pe_eddy eddy_config.txt eddy_indices.txt -export_grad_fsl bvecs bvals -json_export eddy_in.json
+fi
+# RUN EDDY
+slspecfile=$codedir/../sequences/slspec_MB1_60slices_interleaved.txt
+cp $slspecfile slspec.txt
+if [ ! -f ../dwi_den_unr_eddy.mif.gz ]; then
+    eddy_cuda9.1 --imain=eddy_in.nii.gz --mask=eddy_mask_edit.nii.gz --acqp=eddy_config.txt --index=eddy_indices.txt --bvecs=bvecs --bvals=bvals --topup=../topup/field --slm=linear --repol --mporder=8 --s2v_niter=10 --s2v_interp=trilinear --s2v_lambda=1 --estimate_move_by_susceptibility --mbs_niter=20 --mbs_ksp=10 --mbs_lambda=10 --slspec=slspec.txt --out=dwi_post_eddy --verbose
+    # eddy quad
+    #eddy_quad dwi_post_eddy -idx eddy_indices.txt -par eddy_config.txt -b bvals -m eddy_mask_edit.nii.gz -f ../topup/field_fieldmap.nii.gz -s slspec.txt
+    mrconvert dwi_post_eddy.nii.gz ../dwi_den_unr_eddy.mif.gz -strides -1,2,3,4 -fslgrad dwi_post_eddy.eddy_rotated_bvecs bvals
+fi
 
 cd $studydir
 
+##-------------------------------------
 
 ##################################################################################
 # 3. Mask generation, N4 biasfield correction, meanb0 generation and tensor estimation
+
+if [ ! -d $datadir/dwi/preproc/N4 ]; then mkdir -p $datadir/dwi/preproc/N4; fi
+
 cd $datadir/dwi/preproc
 
 echo "Pre-processing with mask generation, N4 biasfield correction, Normalisation, meanb0 generation and tensor estimation"
@@ -260,23 +303,14 @@ echo "Pre-processing with mask generation, N4 biasfield correction, Normalisatio
 dwi=dwi_den_unr_eddy
 
 # Create mask for N4 biasfield correction from b1000
-if [ ! -f N4_mask.mif.gz ]; then
-    # Empirically, meanb2600 produces the best brain mask when extracted with BET at this level
-    #bvalue=2600
-    #dwiextract -shells $bvalue $dwi.mif.gz - | mrmath -force -axis 3 - mean mean${bvalue}tmp.nii.gz
-    #bet mean${bvalue}tmp mean${bvalue}tmp_brain -m -F -R
-    ln -s eddy_mask_edit.mif.gz N4_mask.mif.gz 
-    echo Check result!
-    echo mrview mean${bvalue}tmp.nii.gz -roi.load mean${bvalue}tmp_brain_mask.nii.gz -roi.opacity 0.5 -mode 2
-    mrconvert mean${bvalue}tmp_brain_mask.nii.gz N4_mask.mif.gz
-    rm mean${bvalue}tmp*
+if [ ! -f N4/N4_mask.mif.gz ]; then
+    mrconvert eddy/eddy_mask_edit.nii.gz N4/N4_mask.mif.gz 
 fi
 
 # Do B1-correction. Use ANTs N4
 if [ ! -f  ${dwi}_N4.mif.gz ]; then
     threads=10;
-    if [ ! -d N4 ]; then mkdir N4;fi
-    dwibiascorrect ants -mask N4_mask.mif.gz -bias N4/bias.mif.gz $dwi.mif.gz ${dwi}_N4.mif.gz
+    dwibiascorrect ants -nthreads $threads -mask N4/N4_mask.mif.gz -bias N4/bias.mif.gz $dwi.mif.gz ${dwi}_N4.mif.gz
 fi
 
 
@@ -291,25 +325,14 @@ cd $studydir
 
 cd $datadir/dwi
 
-mrconvert preproc/$dwipreproclast dwi_preproc.mif.gz
+if [ ! -f dwi_preproc.mif.gz ]; then
+    mrconvert preproc/$dwipreproclast dwi_preproc.mif.gz
+fi
 dwi=dwi_preproc
 
 # Create brain mask for normalization 
 if [ ! -f mask.mif.gz ]; then
-    # Empirically, the BET mask of meanb1000 produces the best and tightest brain mask for the cerebrum (not cerebellum)
-    for bvalue in 1000 2600; do 
-	dwiextract -shells $bvalue $dwi.mif.gz - | mrmath -force -axis 3 - mean meanb${bvalue}tmp.nii.gz
-	bet meanb${bvalue}tmp meanb${bvalue}tmp_brain -m -F -R
-    done
-    # Add them together and create a binary mask from union (i.e. voxel value -ge 1)
-    if [ -f meanb2600tmp_brain_mask.nii.gz ]; then # means that we went for union b1000 and b2600
-	mrcalc meanb1000tmp_brain_mask.nii.gz meanb2600tmp_brain_mask.nii.gz -add 1 -ge mask.mif.gz
-    else # we only go for b1000
-	mrconvert meanb1000tmp_brain_mask.nii.gz mask.mif.gz
-    fi
-    echo Check result!
-    echo mrview mask.mif.gz -mode 2
-    rm meanb*tmp*
+    mrconvert preproc/eddy/eddy_mask_edit.nii.gz mask.mif.gz
 fi
 
 # B0-normalisation
